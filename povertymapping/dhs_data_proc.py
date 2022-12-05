6d4303d53788cdc94ac5057c0c3a3d2f177efdd9
Butch Landingin <butch@thinkingmachin.es>
2022-12-05 18:08:15 +0800

    feat: Implement dhs preprocessing

--- povertymapping/dhs_data_proc.py
+++ povertymapping/dhs_data_proc.py
@@ -1,76 +1,67 @@
 import os
-
+import warnings
 import geopandas as gpd
 import pandas as pd
-import yaml
+# import yaml
 from utils.data_utils import aggregate_dhs, get_base_dhs_df
+import geowrangler.dhs as gdhs
+class MissingDataException(Exception):
+    pass
 
-if __name__ == "__main__":
+wealth_col_name = "Wealth Index"
+cluster_col_name = "DHSCLUST"
 
-    config = yaml.safe_load(open("config.yml"))
-    # create output directory
+def dhs_data_proc(config):
+
     save_path = config["save_path"]
     if not os.path.isdir(save_path):
         os.makedirs(save_path)
 
-    # setup upload paths
-    folder_name = config["dhs_folder"]  # maybe avoid this???
-    data_dir = os.path.join(config["data_dir"].split("/")[-1], folder_name)
+    folder_name = config["dhs_folder"]  
     dhs_zip_folder = config["dhs_zip_folder"]
 
-    dvc_folder_name = config["dhs_target_folder_name"]
     repo_path = config["repo_path"]
 
-    # no lambda hook
-    # path_map = lambda x: os.path.join(repo_path, config['data_dir'], x, folder_name)
-
-    def path_map(x):
-        return os.path.join(repo_path, config["data_dir"], x, folder_name)
-
-    reg_path, dvc_path = list(map(path_map, ["", dvc_folder_name]))
+    reg_path = os.path.join(repo_path,config["data_dir"],folder_name )
 
     if os.path.exists(reg_path):
         data_path = reg_path  # or some function of reg_path
-    elif os.path.exists(dvc_path):
-        data_path = dvc_path  # or some function of dvc_path
     else:
-        raise Exception("DHS survey data missing!")
+        raise MissingDataException("DHS survey data missing!")
 
     # get base dhs table
     dhs_file = os.path.join(data_path, dhs_zip_folder, config["dhs_file"])
-    dhs_dict_file = os.path.join(data_path, dhs_zip_folder, config["dhs_dict_file"])
-    dhs = get_base_dhs_df(dhs_file, dhs_dict_file)
+    dhs_df = gdhs.load_dhs_file(dhs_file)
     # check shape
-    print("Data Dimensions: {}".format(dhs.shape))
-    # save base dhs csv
-    dhs.to_csv(os.path.join(save_path, f"{dhs_zip_folder}_base.csv"))
+    print("Data Dimensions: {}".format(dhs_df.shape))
+    # save raw dhs csv
+    dhs_raw_output_file =  f"{dhs_zip_folder}_raw.csv"
+    dhs_df.to_csv(os.path.join(save_path,dhs_raw_output_file))
+
+    # rename columns
+    country = config["country"]
+    column_config = gdhs.load_column_config(country)
+    dhs_df.rename(columns=column_config, inplace=True)
+    dhs_base_output_file =  f"{dhs_zip_folder}_base.csv"
+    dhs_df.to_csv(os.path.join(save_path, dhs_base_output_file))
+
     # aggregate by cluster
-    survey_data = aggregate_dhs(dhs)
+    survey_data = dhs_df[[wealth_col_name, cluster_col_name]].groupby(cluster_col_name).mean()
     print("Data Dimensions: {}".format(survey_data.shape))
 
     # process geospatial
     dhs_geo_zip_folder = config["dhs_geo_zip_folder"]
     dhs_geo_file = config["dhs_geo_file"]
     shp_file_path = os.path.join(data_path, dhs_geo_zip_folder, dhs_geo_file)
-    data = gpd.read_file(shp_file_path)
-
-    # process gps coordinates per cluster
-    cluster_coords = pd.DataFrame(
-        {
-            "DHSID": data["DHSID"],
-            "DHSCLUST": data["DHSCLUST"],
-            "longitude": data["LONGNUM"],
-            "latitude": data["LATNUM"],
-        }
-    )
-    cluster_coords.to_csv(
-        os.path.join(save_path, f"{dhs_geo_zip_folder}_cluster_coords.csv")
+    dhs_shp = gpd.read_file(shp_file_path)
+    dhs_cluster_coords_output_file =  f"{dhs_geo_zip_folder}_cluster_coords.csv"
+    dhs_shp.to_csv(
+        os.path.join(save_path,dhs_cluster_coords_output_file)
     )
 
     # combine survey with geo data
-    survey_geo = pd.merge(survey_data, cluster_coords, on="DHSCLUST")
-    # NOTE: there are 36 clusters without geo location info
-    # save combined base csv
+    survey_geo = pd.merge(survey_data, dhs_shp, on="DHSCLUST")
+    survey_geo_output_file = "{dhs_zip_folder}_{dhs_geo_zip_folder}_by_cluster.csv"
     survey_geo.to_csv(
-        os.path.join(save_path, f"{dhs_zip_folder}_{dhs_geo_zip_folder}_by_cluster.csv")
+        os.path.join(save_path, survey_geo_output_file)
     )
