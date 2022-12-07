dcef17a2c8495ef40d1a18b44cbe11f8c5bb226c
Butch Landingin <butch@thinkingmachin.es>
2022-12-07 16:49:41 +0800

    fix: Update dhs test config to match expected values

--- tests/test_dhs_data_proc.py
+++ tests/test_dhs_data_proc.py
@@ -11,14 +11,14 @@ from povertymapping.dhs_data_proc import process_dhs_data
 def dhsconfig():
     yield dict(
         save_path="test_data/outputs/dhs",
-        repo_path="test_data",
-        data_dir="inputs",
-        dhs_folder="ph",
-        dhs_zip_folder="dhs_ph",
-        dhs_file="ph.DTA",
+        repo_path="test_data/inputs",
+        data_dir="ph",
+        dhs_folder="dhs_ph",
+        dhs_zip_folder="PHHR71DT",
+        dhs_file="PHHR71DT.DTA",
         country="ph",
-        dhs_geo_zip_folder="dhs_ph",
-        dhs_geo_file="ph_gps.shp",
+        dhs_geo_zip_folder="PHGE71FL",
+        dhs_geo_file="PHGE71FL.shp",
     )
 
 
@@ -34,21 +34,21 @@ def test_process_dhs_data(dhsconfig, capsys):
     #
     process_dhs_data(dhsconfig)
     assert (Path(save_path)).exists()
-    assert (Path(save_path) / "dhs_ph_raw.csv").exists()
-    assert (Path(save_path) / "dhs_ph_base.csv").exists()
-    assert (Path(save_path) / "dhs_ph_cluster_coords.csv").exists()
-    assert (Path(save_path) / "dhs_ph_dhs_ph_by_cluster.csv").exists()
-    assert (Path(save_path) / "dhs_ph_dhs_ph_by_cluster.geojson").exists()
+    assert (Path(save_path) / "PHHR71DT_raw.csv").exists()
+    assert (Path(save_path) / "PHHR71DT_base.csv").exists()
+    assert (Path(save_path) / "PHGE71FL_cluster_coords.csv").exists()
+    assert (Path(save_path) / "PHHR71DT_PHGE71FL_by_cluster.csv").exists()
+    assert (Path(save_path) / "PHHR71DT_PHGE71FL_by_cluster.geojson").exists()
 
-    raw = pd.read_csv(Path(save_path) / "dhs_ph_raw.csv")
+    raw = pd.read_csv(Path(save_path) / "PHHR71DT_raw.csv")
     assert raw.shape == (1000, 15)
-    base = pd.read_csv(Path(save_path) / "dhs_ph_base.csv")
+    base = pd.read_csv(Path(save_path) / "PHHR71DT_base.csv")
     assert base.shape == (1000, 15)
-    coords = pd.read_csv(Path(save_path) / "dhs_ph_cluster_coords.csv")
+    coords = pd.read_csv(Path(save_path) / "PHGE71FL_cluster_coords.csv")
     assert coords.shape == (679, 8)
-    cluster = pd.read_csv(Path(save_path) / "dhs_ph_dhs_ph_by_cluster.csv")
+    cluster = pd.read_csv(Path(save_path) / "PHHR71DT_PHGE71FL_by_cluster.csv")
     assert cluster.shape == (679, 9)
-    gdf = gpd.read_file(Path(save_path) / "dhs_ph_dhs_ph_by_cluster.geojson")
+    gdf = gpd.read_file(Path(save_path) / "PHHR71DT_PHGE71FL_by_cluster.geojson")
     assert gdf.shape == (679, 9)
     assert gdf.crs == "EPSG:4326"
     cap = capsys.readouterr()
