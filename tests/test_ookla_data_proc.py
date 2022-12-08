3ae364d53549e35167fe7e0c669b3af3f3cae7d2
Butch Landingin <butch@thinkingmachin.es>
2022-12-08 10:18:13 +0800

    chore: Add missing config params for ookla data processing

--- tests/test_ookla_data_proc.py
+++ tests/test_ookla_data_proc.py
@@ -12,17 +12,28 @@ from povertymapping.ookla_data_proc import process_ookla_data
 def ooklaconfig():
     yield dict(
         save_path="test_data/outputs/ookla",
-        year="2019",
-        quarter="2",
-        repo_path="test_data",
-        boundary_file="new_suco_map",
-        data_dir="inputs",
-        crs="4683",
-        ookla_feature="avg_d_mbps",
+        repo_path="test_data/inputs",
+        data_dir="tl",
+        country="tl",
         ookla_folder="ookla_tl",
         hdx_folder="hdx_tl",
-        country="tl",
         dhs_geo_zip_folder="TLGE71FL",
+        crs="4683",
+        ookla_feature="avg_d_mbps",
+        boundary_file="new_suco_map",
+        year="2019",
+        quarter="2",
+        sample=False,
+        random_sample=False,
+        no_samples=60,
+        random_seed=42,
+        clust_rad=2000,
+        plot_ookla_features=True,
+        adm_level=3,
+        use_pcode=False,
+        shape_label='SUCO_NUMCD',
+        bins=6,
+        show_legend=False,
     )
 
 def test_process_ookla_data(ooklaconfig, capsys):
@@ -32,17 +43,14 @@ def test_process_ookla_data(ooklaconfig, capsys):
     # setup dhs data
     os.makedirs(Path(save_path))
     # copy dhs preproc data from tl process_dhs_data - TLGE71FL_cluster_coords.csv
-    shutil.copy("test_data/real_outputs/dhs_tl/TLGE71FL_cluster_coords.csv",Path(save_path)/"TLGE71FL_cluster_coords.csv")
+    shutil.copy("test_data/real_outputs/dhs_tl/TLGE71FL_cluster_coords.csv",(Path(save_path)/"TLGE71FL_cluster_coords.csv").as_posix())
     # to save_path
     process_ookla_data(ooklaconfig)
-    assert Path(save_path/"tl_2019_2_avg_d_mbps.csv").exists()
-    # assert (Path(save_path)).exists()
-    # assert (Path(save_path) / "dhs_ph_raw.csv").exists()
-    # assert (Path(save_path) / "dhs_ph_base.csv").exists()
-    # assert (Path(save_path) / "dhs_ph_cluster_coords.csv").exists()
-    # assert (Path(save_path) / "dhs_ph_dhs_ph_by_cluster.csv").exists()
-    # assert (Path(save_path) / "dhs_ph_dhs_ph_by_cluster.geojson").exists()
+    assert Path(save_path).exists()
+    assert (Path(save_path)/"tl_2019_2_avg_d_mbps.csv").exists()
+    assert (Path(save_path)/"new_suco_map_avg_d_mbps.jpeg").exists()
 
+    # TODO: Fix path where jpeg is saved
     # raw = pd.read_csv(Path(save_path) / "dhs_ph_raw.csv")
     # assert raw.shape == (1000, 15)
     # base = pd.read_csv(Path(save_path) / "dhs_ph_base.csv")
