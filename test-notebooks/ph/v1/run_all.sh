fe2e1e9ca1dde77ee24965f09011184e8db6afd0
alron <alron@thinkingmachin.es>
2023-01-03 23:01:45 +0800

    Add template folder for structuring an end-to-end model experiment

---
+++ test-notebooks/ph/v1/run_all.sh
@@ -0,0 +1,4 @@
+jupyter nbconvert --execute --to notebook --inplace 1_prepare_data.ipynb
+jupyter nbconvert --execute --to notebook --inplace 2_train_model.ipynb
+jupyter nbconvert --execute --to notebook --inplace 3_rollout_model.ipynb
+jupyter nbconvert --execute --to notebook --inplace 4_eda_rollout_results.ipynb
