cb533df31dad85f4288879a7637c5acf80582824
alron <alron@thinkingmachin.es>
2023-01-09 23:31:41 +0800

    feat: Add function for generating OSM road features
    Also added a convenience class for managing the caching of OSM data.

--- povertymapping/osm.py
+++ povertymapping/osm.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import uuid
 from pathlib import Path
 from zipfile import ZipFile
 
@@ -9,26 +10,121 @@ from geowrangler import vector_zonal_stats as vzs
 from geowrangler.datasets import geofabrik
 from loguru import logger
 
+from povertymapping import settings
+
 DEFAULT_POI_TYPES = ["restaurant", "school", "bank", "supermarket", "mall", "atm"]
 
 
-def add_osm_features(
+class OsmDataManager:
+    """An instance of this class provides convenience functoins for loading and caching OSM data"""
+
+    DEFAULT_CACHE_DIR = "~/.geowrangler/osm"
+
+    def __init__(self, cache_dir=DEFAULT_CACHE_DIR):
+        self.pois_cache = {}
+        self.roads_cache = {}
+        self.cache_dir = cache_dir
+
+    def load_pois(self, country, use_cache=True):
+        # Get from RAM cache if already available
+
+        logger.debug(f"Contents of POIs cache: {list(self.roads_cache.keys())}")
+        if country in self.pois_cache:
+            logger.debug(f"OSM POIs for {country} found in cache.")
+            return self.pois_cache[country]
+
+        # Otherwise, load from file and add to cache
+        country_cache_dir = download_osm_country_data(
+            country,
+            cache_dir=self.cache_dir,
+            use_cache=use_cache,
+        )
+        osm_pois_filepath = os.path.join(country_cache_dir, "gis_osm_pois_free_1.shp")
+        logger.debug(f"OSM POIs for {country} being loaded from {osm_pois_filepath}")
+        gdf = gpd.read_file(osm_pois_filepath)
+        self.pois_cache[country] = gdf
+
+        return gdf
+
+    def load_roads(self, country, use_cache=True):
+        # Get from RAM cache if already available
+        logger.debug(f"Contents of roads cache: {list(self.roads_cache.keys())}")
+        if country in self.roads_cache:
+            logger.debug(f"OSM Roads for {country} found in cache.")
+            return self.roads_cache[country]
+
+        # Otherwise, load from file and add to cache
+        country_cache_dir = download_osm_country_data(
+            country,
+            cache_dir=self.cache_dir,
+            use_cache=use_cache,
+        )
+        osm_roads_filepath = os.path.join(country_cache_dir, "gis_osm_roads_free_1.shp")
+        logger.debug(f"OSM Roads for {country} being loaded from {osm_roads_filepath}")
+        gdf = gpd.read_file(osm_roads_filepath)
+        self.roads_cache[country] = gdf
+
+        return gdf
+
+
+def download_osm_country_data(country, cache_dir, use_cache=True):
+
+    osm_cache_dir = os.path.join(cache_dir, "osm/")
+    # TODO consider incorporating year or quarter to automatically avoid using stale data
+    country_cache_dir = os.path.join(osm_cache_dir, country)
+
+    # Check if the cached data is valid. Otherwise, we have to re-download.
+    # Temporary quick check now is to see if the country cache folder is non-empty.
+    # TODO: Can improve this later if we need more specific validity checks.
+    cached_data_available = (
+        os.path.exists(country_cache_dir) and len(os.listdir(country_cache_dir)) > 0
+    )
+
+    logger.info(
+        f"OSM Data: Cached data available for {country} at {country_cache_dir}? {cached_data_available}"
+    )
+
+    # Download if cache is invalid or user specified use_cache = False
+    if not cached_data_available or not use_cache:
+        logger.info(
+            f"OSM Data: Re-initializing OSM country cache dir at {country_cache_dir}..."
+        )
+        # Re-create the country cache dir and start over to fix any corrupted states
+        shutil.rmtree(country_cache_dir, ignore_errors=True)
+        Path(country_cache_dir).mkdir(parents=True, exist_ok=True)
+
+        # This downloads a zip file to the country cache dir
+        logger.info(f"OSM Data: Downloading Geofabrik zip file...")
+        zipfile_path = geofabrik.download_geofabrik_region(country, country_cache_dir)
+
+        # Unzip the zip file
+        logger.info(f"OSM Data: Unzipping the zip file...")
+        with ZipFile(zipfile_path, "r") as zip_object:
+            zip_object.extractall(country_cache_dir)
+
+        # Delete the zip file
+        os.remove(zipfile_path)
+
+        logger.info(
+            f"OSM Data: Successfully downloaded and cached OSM data for {country} at {country_cache_dir}!"
+        )
+
+    return country_cache_dir
+
+
+def add_osm_poi_features(
     aoi,
     country,
-    cache_dir,
+    osm_data_manager,
+    use_cache=True,
     poi_types=DEFAULT_POI_TYPES,
     metric_crs="epsg:3123",
     inplace=False,
-    force_overwrite=False,
 ):
-    """Generates features for the AOI based on OSM data (POIs, roads, etc)."""
+    """Generates features for the AOI based on OSM POI data (POIs, roads, etc)."""
 
     # Load-in the OSM POIs data
-    osm_cache_dir = download_osm_country_data(
-        country, cache_dir, force_overwrite=force_overwrite
-    )
-    osm_pois_filepath = os.path.join(osm_cache_dir, "gis_osm_pois_free_1.shp")
-    osm = gpd.read_file(osm_pois_filepath)
+    osm = osm_data_manager.load_pois(country, use_cache=use_cache)
 
     # Create a copy of the AOI gdf if not inplace to avoid modifying the original gdf
     if not inplace:
@@ -70,46 +166,32 @@ def add_osm_features(
     return aoi
 
 
-def download_osm_country_data(country, cache_dir, force_overwrite=False):
+def add_osm_road_features(
+    aoi, country, osm_data_manager, use_cache=True, inplace=False
+):
+    roads_gdf = osm_data_manager.load_roads(country, use_cache=use_cache)
+    assert aoi.crs == roads_gdf.crs
 
-    osm_cache_dir = os.path.join(cache_dir, "osm/")
-    # TODO consider incorporating year or quarter to automatically avoid using stale data
-    country_cache_dir = os.path.join(osm_cache_dir, country)
+    if not inplace:
+        aoi = aoi.copy()
 
-    # Check if the cached data is valid. Otherwise, we have to re-download.
-    # Temporary quick check now is to see if the country cache folder is non-empty.
-    # TODO: Can improve this later if we need more specific validity checks.
-    cached_data_available = (
-        os.path.exists(country_cache_dir) and len(os.listdir(country_cache_dir)) > 0
-    )
+    # Create a temporary ID column to use for our processing
+    # Assign simple integer values
+    temp_id = str(uuid.uuid4())
+    aoi[temp_id] = list(range(len(aoi)))
 
-    logger.info(
-        f"OSM Data: Cached data available for {country} at {country_cache_dir}? {cached_data_available}"
-    )
+    # Get intersections between the AOIs and the roads
+    joined = gpd.sjoin(aoi, roads_gdf, how="inner", predicate="intersects")
 
-    # Download if cache is invalid or user specified force_overwrite=True
-    if not cached_data_available or force_overwrite:
-        logger.info(
-            f"OSM Data: Re-initializing OSM country cache dir at {country_cache_dir}..."
-        )
-        # Re-create the country cache dir and start over to fix any corrupted states
-        shutil.rmtree(country_cache_dir, ignore_errors=True)
-        Path(country_cache_dir).mkdir(parents=True, exist_ok=True)
+    # Count the number of roads that intersected
+    road_stats_df = joined.groupby(temp_id)["index_right"].agg(road_count="count")
+    # Join back to the original AOI gdf
+    aoi = aoi.join(road_stats_df, how="left", on=temp_id)
 
-        # This downloads a zip file to the country cache dir
-        logger.info(f"OSM Data: Downloading Geofabrik zip file...")
-        zipfile_path = geofabrik.download_geofabrik_region(country, country_cache_dir)
+    # There might be AOIs that did not intersect with any roads at all
+    aoi["road_count"] = aoi["road_count"].fillna(0)
 
-        # Unzip the zip file
-        logger.info(f"OSM Data: Unzipping the zip file...")
-        with ZipFile(zipfile_path, "r") as zip_object:
-            zip_object.extractall(country_cache_dir)
+    # Remove the temporary ID column
+    del aoi[temp_id]
 
-        # Delete the zip file
-        os.remove(zipfile_path)
-
-        logger.info(
-            f"OSM Data: Successfully downloaded and cached OSM data for {country} at {country_cache_dir}!"
-        )
-
-    return country_cache_dir
+    return aoi
