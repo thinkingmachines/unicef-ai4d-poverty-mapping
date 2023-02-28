72ab68c2773a663c8cd03425638f1746fb94301a
Butch Landingin <butch@thinkingmachin.es>
2023-02-28 18:16:36 +0800

    chore: Add slow tests for batching/parallelization for raster zonal stats

--- tests/test_rollout_grids.py
+++ tests/test_rollout_grids.py
@@ -12,3 +12,16 @@ def test_get_region_filtered_bingtile_grids_with_parallel(tmpdir):
     cache_dir = str(tmpdir/'this-directory-does-not-exist')
     gdf = get_region_filtered_bingtile_grids('timor-leste', cache_dir=cache_dir, max_batch_size=50_000, n_workers=8)
     assert len(gdf) == 2024
+
+@pytest.mark.slow
+def test_get_region_filtered_bingtile_grids_batched(tmpdir):
+    cache_dir = str(tmpdir/'this-directory-does-not-exist')
+    gdf = get_region_filtered_bingtile_grids('timor-leste', cache_dir=cache_dir, max_batch_size=351)
+    assert len(gdf) == 2024
+
+@pytest.mark.slow
+def test_get_region_filtered_bingtile_grids_with_batched_parallel(tmpdir):
+    cache_dir = str(tmpdir/'this-directory-does-not-exist')
+    gdf = get_region_filtered_bingtile_grids('timor-leste', cache_dir=cache_dir, max_batch_size=700, n_workers=4)
+    assert len(gdf) == 2024
+
