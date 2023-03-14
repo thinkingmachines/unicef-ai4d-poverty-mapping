8ffd7624e44bbba9daef019c96efddfffb0ebec8
Jace Peralta <jcp@thinkingmachin.es>
2023-03-14 11:27:08 +0800

    Add arg to return 2-character country code

--- povertymapping/iso3.py
+++ povertymapping/iso3.py
@@ -13,10 +13,17 @@ def get_iso3_codes():
     iso3_lookup = {item['name'].lower(): item for item in iso3_df.to_dict('records')}
     return iso3_lookup
 
-def get_iso3_code(region):
+def get_iso3_code(region, code='alpha-3'):
+    """
+    Returns the country ISO-3166 code
+    Args:
+        region (str): Common short country name. Refer to https://data.worldbank.org/country for possible values
+        code (str): The country code standard, either 'alpha-3' or 'alpha-2'
+    """
     # TODO: Find more elegant solution to correct country common name to ISO standard spelling
     iso_standard_region_name_lookup = {"vietnam": "viet nam",
-                            "laos": "lao people's democratic republic"}
+                            "laos": "lao people's democratic republic",
+                            "lao pdr": "lao people's democratic republic"}
     iso3_lookup = get_iso3_codes()
     iso3_entry = iso3_lookup.get(iso_standard_region_name_lookup.get(region, region))
-    return iso3_entry['alpha-3'] if iso3_entry is not None else None
+    return iso3_entry[code] if iso3_entry is not None else None
