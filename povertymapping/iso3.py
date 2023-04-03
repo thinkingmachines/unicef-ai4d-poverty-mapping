0fa7310cabdd84394c5aeabd565ceb102eb3d6e6
Jace Peralta <jcp@thinkingmachin.es>
2023-04-03 08:45:37 +0800

    modify iso3 module and fix dependencies

--- povertymapping/iso3.py
+++ povertymapping/iso3.py
@@ -1,30 +1,44 @@
 
 from functools import lru_cache
 import pandas as pd
+import warnings
 # ISO3166 source list
 # see https://github.com/lukes//ISO-3166-Countries-with-Regional-Codes
 
-ISO3_URL = 'https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv'
-
+ISO3_URL = "https://datahub.io/core/country-codes/r/country-codes.csv"
 
 @lru_cache(maxsize=None)
 def get_iso3_codes():
     iso3_df = pd.read_csv(ISO3_URL)
-    iso3_lookup = {item['name'].lower(): item for item in iso3_df.to_dict('records')}
+    iso3_df.columns = [col.lower().replace('_','-').replace(' ','-') for col in iso3_df.columns]
+    iso3_lookup = iso3_df[['iso3166-1-alpha-3','cldr-display-name']].apply(lambda x: x.astype(str).str.lower())
+    iso3_lookup['cldr-display-name'] = iso3_lookup['cldr-display-name'].str.replace(' ','-')
+    iso3_lookup['iso-alpha-2'] = iso3_lookup['iso3166-1-alpha-3'].apply(lambda x: x[:-1])
+    iso3_lookup.columns = ['iso-alpha-3','common-name','iso-alpha-2']
+    
     return iso3_lookup
 
-def get_iso3_code(region, code='alpha-3'):
-    """
-    Returns the country ISO-3166 code
-    Args:
-        region (str): Common short country name. Refer to https://data.worldbank.org/country for possible values
-        code (str): The country code standard, either 'alpha-3' or 'alpha-2'
-    """
-    # TODO: Find more elegant solution to correct country common name to ISO standard spelling
-    iso_standard_region_name_lookup = {"vietnam": "viet nam",
-                            "laos": "lao people's democratic republic",
-                            "lao pdr": "lao people's democratic republic",
-                            "east-timor": "timor-leste"}
+def is_valid_iso3_code(iso_country_code, code='alpha-2'):
     iso3_lookup = get_iso3_codes()
-    iso3_entry = iso3_lookup.get(iso_standard_region_name_lookup.get(region, region))
-    return iso3_entry[code] if iso3_entry is not None else None
+    return True if iso_country_code in iso3_lookup['iso-'+code].values else False
+
+def is_valid_country_name(country_common_name):
+    iso3_lookup = get_iso3_codes()
+    return True if country_common_name in iso3_lookup['common-name'].values else False
+
+def get_region_name(iso_country_code, code='alpha-2'):
+    iso3_lookup = get_iso3_codes()
+    if is_valid_iso3_code(iso_country_code, code):
+        return iso3_lookup[iso3_lookup['iso-'+code]==iso_country_code]['common-name'].values[0]
+    else:
+        warnings.warn(f'Invalid iso3 code. Head to https://www.iso.org/iso-3166-country-codes.html to check the correct code.')
+        return None
+
+def get_iso3_code(country_common_name, code='alpha-2'):
+    iso3_lookup = get_iso3_codes()
+    if is_valid_country_name(country_common_name):
+        return iso3_lookup[iso3_lookup['common-name']==country_common_name]['iso-'+code].values[0]
+    else:
+        warnings.warn(f'Country {country_common_name} not found. Head to https://www.iso.org/iso-3166-country-codes.html to check the correct country name.')
+        return None
+
