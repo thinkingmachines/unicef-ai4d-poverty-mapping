077dfcadcec978c54fedb607fe1b8407b064a5d2
alron <alron@thinkingmachin.es>
2023-04-17 20:58:11 +0800

    Fix tests
    Aside from updating tests for the new iso3 functions, the function mockers had a bug.
    Specific error: running test_rollout_grids.py by itself is ok, but running the tests as a whole will fail.
    Root cause was found to have been the mock patches for function return values in test_geoboundaries and test_iso3.
    It was hard to find a fix, so decided to remove the mocking instead.
    The functions run quickly anyway, and this way, we can actually test real API return values.

--- tests/test_geoboundaries.py
+++ tests/test_geoboundaries.py
@@ -1,34 +1,15 @@
+import uuid
+
 from povertymapping.geoboundaries import get_geoboundaries
 
-import pytest
-import pandas as pd
 
-
-@pytest.fixture
-def mock_geobounds_req(mocker):
-    mock_df = pd.DataFrame.from_records(
-        [
-            {
-                "name": "Afghanistan",
-                "alpha-3": "AFG",
-            }
-        ]
-    )
-    mocker.patch("pandas.read_csv", return_value=mock_df)
-    mock_resp = mocker.MagicMock()
-    return_value = [{"gjDownloadURL": "https://example.com/afghanistan.geojson"}]
-    mock_resp.json = mocker.MagicMock(return_value=return_value)
-    mocker.patch("requests.get", return_value=mock_resp)
-    yield
-
-
-def test_get_geoboundaries(tmpdir, mocker, mock_geobounds_req):
+def test_get_geoboundaries(tmpdir, mocker):
     mock_file = mocker.MagicMock()
     expected_return = (mock_file, None, None)
     mocker.patch(
         "povertymapping.geoboundaries.urlretrieve", return_value=expected_return
     )
-    mock_cache_dir = str(tmpdir / "this-directory-does-not-exist")
+    mock_cache_dir = str(tmpdir / str(uuid.uuid4()))
 
     result = get_geoboundaries("afghanistan", cache_dir=mock_cache_dir)
     assert result is mock_file
--- tests/test_rollout_grids.py
+++ tests/test_rollout_grids.py
@@ -1,27 +1,37 @@
-from povertymapping.rollout_grids import get_region_filtered_bingtile_grids
 import pytest
 
+from povertymapping.rollout_grids import get_region_filtered_bingtile_grids
+
+
 @pytest.mark.slow
-def test_get_region_filtered_bingtile_grids(mocker, tmpdir):
-    cache_dir = str(tmpdir/'this-directory-does-not-exist')
-    gdf = get_region_filtered_bingtile_grids('timor-leste', cache_dir=cache_dir)
+def test_get_region_filtered_bingtile_grids(tmpdir):
+    cache_dir = str(tmpdir / "this-directory-does-not-exist")
+    gdf = get_region_filtered_bingtile_grids("timor-leste", cache_dir=cache_dir)
     assert len(gdf) == 2024
+
 
 @pytest.mark.slow
 def test_get_region_filtered_bingtile_grids_with_parallel(tmpdir):
-    cache_dir = str(tmpdir/'this-directory-does-not-exist')
-    gdf = get_region_filtered_bingtile_grids('timor-leste', cache_dir=cache_dir, max_batch_size=50_000, n_workers=8)
+    cache_dir = str(tmpdir / "this-directory-does-not-exist")
+    gdf = get_region_filtered_bingtile_grids(
+        "timor-leste", cache_dir=cache_dir, max_batch_size=50_000, n_workers=8
+    )
     assert len(gdf) == 2024
+
 
 @pytest.mark.slow
 def test_get_region_filtered_bingtile_grids_batched(tmpdir):
-    cache_dir = str(tmpdir/'this-directory-does-not-exist')
-    gdf = get_region_filtered_bingtile_grids('timor-leste', cache_dir=cache_dir, max_batch_size=351)
+    cache_dir = str(tmpdir / "this-directory-does-not-exist")
+    gdf = get_region_filtered_bingtile_grids(
+        "timor-leste", cache_dir=cache_dir, max_batch_size=351
+    )
     assert len(gdf) == 2024
+
 
 @pytest.mark.slow
 def test_get_region_filtered_bingtile_grids_with_batched_parallel(tmpdir):
-    cache_dir = str(tmpdir/'this-directory-does-not-exist')
-    gdf = get_region_filtered_bingtile_grids('timor-leste', cache_dir=cache_dir, max_batch_size=700, n_workers=4)
+    cache_dir = str(tmpdir / "this-directory-does-not-exist")
+    gdf = get_region_filtered_bingtile_grids(
+        "timor-leste", cache_dir=cache_dir, max_batch_size=700, n_workers=4
+    )
     assert len(gdf) == 2024
-
