5f0da03257813b54b685197fe876e3ab2fb6a305
Butch Landingin <butch@thinkingmachin.es>
2023-06-09 17:13:46 +0800

    feat:Add prompter to fillout data for model rollout

--- scripts/run_rollout.py
+++ scripts/run_rollout.py
@@ -1,114 +1,128 @@
 import papermill as pm
-import json
 import os
 import yaml
+from datetime import datetime
+from PyInquirer.prompt import prompt
+from PyInquirer import print_json
+import povertymapping.iso3 as iso3 
 
 RUN_CODE = True
 DEBUG = True
 
+
 def exec_pm(*args, **kwargs):
     debug = kwargs.pop("debug", False)
     if debug:
-        print(f"env: EOG_USER {os.environ.get('EOG_USER','None')} EOG_PASSWORD {os.environ.get('EOG_PASSWORD','None')}")
-        print(f"args: {json.dumps(args)}\n kwargs: {json.dumps(kwargs)}")
+        print_json(dict(where="exec_pm"))
+        print_json(dict(environ=dict(EOG_USER=os.environ.get('EOG_USER',None), EOG_PASSWORD=os.environ.get('EOG_PASSWORD',None))))
+        print_json(dict(args=args))
+        print_json(dict(kwargs=kwargs))
         return None
     return pm.execute_notebook(*args, **kwargs)
 
+def override_defaults(questions, answers, update_list):
+    for name in update_list:
+        question = next(
+            (q for q in questions if q["name"] == name), None
+        )
+        if question:
+            question["default"] = answers[name]
 
 def main():
     if not RUN_CODE:
         print("Hello, run rollout world!")
         return
 
-    print("Rolling out!")
+    print("Answer the following questions")
 
     with open("scripts/config.yaml") as f:
-        config = yaml.load(f,Loader=yaml.SafeLoader)
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+    with open("scripts/config_rollout.yaml") as f:
+        config_rollout = yaml.load(f, Loader=yaml.SafeLoader)
 
-    country_name = ""
-    rollout_date = ""
-    notebook_type = "single-country"
-    country_code = ""
-    model_gdrive_url = ""
-    country_osm = ""
-    dhs_dta_prefix = ""
-    dhs_geo_prefix = ""
-
-    # prompt for notebook type (single-country, cross-country)
-    # prompt for task_type (rollout, train, all)
+    answers = dict(
+        eog_user_id="",
+        eog_password="",
+        country_name="",
+        rollout_date="",
+        country_code="",
+        model_weights_url="",
+        country_osm="",
+        ookla_year = "",
+        nightlights_year = "",
+    )
+    # prompt for eog_user
+    # prompt for eog_password # password
     # prompt for country
     # prompt for rollout_date
-    
-    defaults = filter(lambda x: x["name"] == country_name, config.get(notebook_type,[]))
-    defaults = list(defaults)
-    if defaults:
-        default_config = defaults[0]
-        country_code = default_config.get("code","")
-        country_osm = default_config.get("country-osm","")
-        model_gdrive_url = default_config.get("model-weights-url")
-        dhs_dta_prefix = default_config.get("dhs-stata-prefix","")
-        dhs_geo_prefix = default_config.get("dhs-geo-prefix","")
-    
-    # prompt for country-code, country-osm, model-gdrive-url, dhs-dta-prefix, dhs-geo-prefix
-    task_type = "rollout"
-
-    # depending on task type
-    eog_user_id = "eog_user_id"
-    eog_password = "eog_password"
-    ookla_year = "2019"
-    nightlights_year = "2016"
-
-    os.environ['EOG_USER'] = eog_user_id
-    os.environ['EOG_PASSWORD'] = eog_password
-    task_stages = dict(
-        rollout=["2_generate_grids", "3_rollout_model"],
-        train=["0_generate_training_data", "1_train_model"],
-        all=["0_generate_training_data", "1_train_model", "2_generate_grids", "3_rollout_model"]
+    questions1 = config_rollout.get("questions-part1")
+    # override defaults
+    rollout_date_question = next(
+        (q for q in questions1 if q["name"] == "rollout_date"), None
     )
+    if rollout_date_question:
+        rollout_date_question["default"] = datetime.now().strftime("%Y-%m-%d")
+
+    # print_json(dict(questions=questions))
+    answers = prompt(questions1, answers=answers)
+    # print_json(dict(answers=answers))
+
+    single_country_models = config.get("single_country_models",[])
+    default_config = next((c for c in single_country_models if c["name"] == answers["country_name"]), None)
+    cross_country_config = config.get("cross_country_model",None)
+
+    if default_config:
+        answers["country_code"] = default_config.get("country_code", "")
+        answers["country_osm"] = default_config.get("country_osm", "")
+        answers["model_weights_url"] = default_config.get("model_weights_url", "")
+        answers["ookla_year"] = default_config.get("ookla_year", "")
+        answers["nightlights_year"] = default_config.get("nightlights_year", "")
+        model_type = "single-country"
+    else:
+        model_type = "cross-country"
+        answers["country_code"] = iso3.get_iso3_code(answers["country_name"])
+        answers["country_osm"] = answers["country_name"]
+        if cross_country_config:
+            answers["model_weights_url"] = cross_country_config.get("model_weights_url", "")
+
+    print(f"Model type: {model_type}")
+    # prompt for country-code, country-osm, model-gdrive-url, nightlights-year, ookla-year
+    questions2 = config_rollout.get("questions-part2")
+    override_defaults(questions2, answers, ["country_code", "country_osm", "model_weights_url", "nightlights_year", "ookla_year"])
+    answers = prompt(questions2, answers=answers)
+
+    os.environ["EOG_USER"] = answers["eog_user_id"]
+    os.environ["EOG_PASSWORD"] = answers["eog_password"]
+    stages = ["2_generate_grids", "3_rollout_model"]
     stage_params = {
-        "0_generate_training_data": dict(
-                                        COUNTRY_CODE=country_code,
-                                        COUNTRY_OSM=country_osm,
-                                        ROLLOUT_DATE=rollout_date,
-                                        DHS_DTA_PREFIX=dhs_dta_prefix,
-                                        DHS_GEO_PREFIX=dhs_geo_prefix,
-
-                                        ),
-        "1_train_model": dict(
-                                COUNTRY_CODE=country_code,  
-                                ROLLOUT_DATE=rollout_date,
-                                COUNTRY_OSM=country_osm,
-                                OOKLA_YEAR=ookla_year,
-                                NIGHTLIGHTS_YEAR=nightlights_year,
-                            ),
         "2_generate_grids": dict(
-                                COUNTRY_CODE=country_code, 
-                                ROLLOUT_DATE=rollout_date,
-                                ),
-        "3_rollout_model": dict(COUNTRY_CODE=country_code, 
-                                ROLLOUT_DATE=rollout_date,
-                                COUNTRY_OSM=country_osm,
-                                OOKLA_YEAR=ookla_year,
-                                NIGHTLIGHTS_YEAR=nightlights_year,
-                                # model_gdrive_url is None for task_type == "all"
-                                MODEL_GDRIVE_URL=model_gdrive_url if task_type == "rollout" else None 
-                                )
+            COUNTRY_CODE=answers["country_code"],
+            ROLLOUT_DATE=answers["rollout_date"],
+        ),
+        "3_rollout_model": dict(
+            COUNTRY_CODE=answers["country_code"],
+            ROLLOUT_DATE=answers["rollout_date"],
+            COUNTRY_OSM=answers["country_osm"],
+            OOKLA_YEAR=answers["ookla_year"],
+            NIGHTLIGHTS_YEAR=answers["nightlights_year"],
+            MODEL_WEIGHTS_URL=answers["model_weights_url"],
+        ),
     }
-    # get parameters
-    stages = task_stages.get(task_type, [])
 
-    input_notebook_dir = f"notebooks/{notebook_type}"
+    input_notebook_dir = f"notebooks/{model_type}"
+    if model_type == "cross-country" and answers["country_code"] == "id": 
+        # special case indonesia due to grid size
+        input_notebook_dir = "notebooks/cross_country/id"
 
-    output_notebook_dir = f"output-notebooks/{notebook_type}"
-    
+    output_notebook_dir = f"output-notebooks/{model_type}"
     # execute
     for stage in stages:
-        output_stage = f"{country_code}_{stage}"
+        output_stage = f"{answers['country_code']}_{stage}"
         exec_pm(
             f"{input_notebook_dir}/{stage}.ipynb",
             f"{output_notebook_dir}/{output_stage}.ipynb",
             parameters=stage_params[stage],
-            debug=DEBUG
+            debug=DEBUG,
         )
 
 
